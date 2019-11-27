1c00c56c6e53963f43127926512afaa6df14ebd8
FelipeSVidal <felipedesousavidal@gmail.com>
2019-11-27 13:47:31 -0300

    Fix adding await

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const app = dialogflow({
     debug: true
   });
 
-app.intent("Todos os Cursos", function(conv) {
+app.intent("Todos os Cursos", async function(conv) {
     conv.ask("Lista de Todos os Cursos");
     let options = {
         method: 'get',
@@ -29,7 +29,7 @@ app.intent("Todos os Cursos", function(conv) {
         'bearer': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyVG9rZW4iOiJmNDRjM2JjMzI3YTkwODJmMTY2NmRlMGJiN2YwOGFhYiIsImFkbWluVG9rZW4iOiJmNjA2MDdmNDUyZTU0ZGQ1ZTU5ZmI5ZjNmNTA0YWIzYyIsInVzZXJJZCI6MTksImlhdCI6MTU3MzU5MTkzNH0.B2vr1OxHFdkfIrMzHu3wZq6Ozy5IqJqDkX205kRz_0Q'
     }
 
-    let courses = request(options).then(function(res){return res}).catch(function(err){console.log('erro in courses', err)});
+    let courses = await request(options).then(function(res){return res}).catch(function(err){console.log('erro in courses', err)});
 
     let columns = [];
     let rows = [];
